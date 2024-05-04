022db611f32938171b260403c265b6e26d52a3a5
朱天文 <8375170+zzztw@user.noreply.gitee.com>
2024-05-04 16:05:43 +0800

    token控制路由权限

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,12 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import {createBrowserRouter} from 'react-router-dom'
+import { AuthRoute } from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element:<Layout />
+    element:<AuthRoute><Layout /></AuthRoute>
   },
   {
     path: '/login',
