4022fe69b3c30b69f170580e3f3a3f87ec93c090
朱天文 <8375170+zzztw@user.noreply.gitee.com>
2024-05-05 20:34:45 +0800

    收集表单数据并提交表单

--- src/apis/article.js
+++ src/apis/article.js
@@ -8,3 +8,12 @@ export function getChannelAPI () {
     method: 'GET'
   })
 }
+
+// 提交文章表单
+export function createArticleAPI(data) {
+  return request({
+    url: '/mp/articles?draft=false',
+    method: 'POST',
+    data
+  })
+}
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -15,7 +15,7 @@ import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useState } from 'react'
-import { getChannelAPI } from '@/apis/article'
+import { createArticleAPI, getChannelAPI } from '@/apis/article'
 
 const { Option } = Select
 
@@ -30,6 +30,24 @@ const Publish = () => {
     }
     getChannelList()
   },[])
+
+  // 提交表单
+  const onFinish = (formValue) => {
+    // 按照接口文档的格式处理收集到的数据
+    const {title, content, channel_id} = formValue
+    const reqData = {
+      title,
+      content,
+      cover: {
+        type: 0,
+        images: []
+      },
+      channel_id: ''
+    }
+
+    // 调用接口
+    createArticleAPI(reqData)
+  }
   return (
     <div className="publish">
       <Card
@@ -46,6 +64,7 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish={onFinish}
         >
           <Form.Item
             label="标题"
